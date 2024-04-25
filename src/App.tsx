6b870095fcb7fa1f8d32d7f1c01ef7902a4f99dc
Kebin <kebinwang@gmail.com>
2024-04-25 01:23:55 -0400

    Add datagrid loading from restapi. Still need to workaround CORS

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import DataHandler from './DataHandler';
+import DataTable from './component/DataTable'
 
 function App() {
- return <DataHandler />
+ return <DataTable />
 }
 
 export default App;
---
+++ src/component/DataTable.tsx
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react'
+import Box from '@mui/material/Box'
+import {DataGrid, GridColDef} from '@mui/x-data-grid'
+
+const columns = [
+    {field: 'id', headerName: 'ID'},
+    {field: 'loc', headerName: 'Location'},
+    {field: 'size', headerName: 'Size'},
+    {field: 'name', headerName:'FileName'}
+]
+
+const DataTable = () => {
+    const [tableData, setTableData] = useState([])
+
+    const reqOptions = {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'}
+        body: JSON.stringify({'search':'teststring'})
+    }
+    useEffect(()=>{
+          //fetch("https://jsonplaceholder.typicode.com/posts")
+        fetch("http://localhost:8082/v1/search", reqOptions)
+        .then((data) => data.json())
+        .then((data) => setTableData(data))
+    })
+
+    return (
+        <div>
+            <h2>Data Table</h2>
+            <DataGrid
+                rows={tableData}
+                columns={columns}
+            />
+        </div>
+    )
+}
+
+export default DataTable
