4a3ade1c14ab8228fe53021376ef36c5bdfa6994
Kebin <kebinwang@gmail.com>
2024-05-29 10:02:07 -0400

    row click handler

--- src/component/DataTable.tsx
+++ src/component/DataTable.tsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
+import Alert from '@mui/material/Alert';
 import Box from '@mui/material/Box'
-import {DataGrid, GridColDef} from '@mui/x-data-grid'
+import {DataGrid, GridEventListener,GridColDef} from '@mui/x-data-grid'
 
 const columns = [
     {field: 'id', headerName: 'ID'},
@@ -9,13 +10,24 @@ const columns = [
     {field: 'name', headerName:'FileName'}
 ]
 
+const handleRowClick: GridEventListener<'rowClick'> = (params) => {
+    alert(`row "${params.row.name}" clicked`);
+  };
+
+
+const cntcols = [
+    {field: 'size', headerName: 'Size'},
+    {field: 'name', headerName:'FileName'},
+    {field: 'count', headerName:'Count'}
+]
+
 const DataTable = () => {
     const [tableData, setTableData] = useState([])
 
     const reqOptions = {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({'search':'string'})
+        body: JSON.stringify({'search':''})
     }
     useEffect(()=>{
         fetch("http://localhost:8082/v1/search", reqOptions)
@@ -29,6 +41,7 @@ const DataTable = () => {
             <DataGrid
                 rows={tableData}
                 columns={columns}
+                onRowClick={handleRowClick} 
             />
         </div>
     )
